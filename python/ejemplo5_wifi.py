636eefde62f1e0acb29838a3c6ee2d59b411a514
javacasm <javacasm@gmail.com>
2023-09-12 19:00:46 +0200

    ejemplo wifi

--- python/ejemplo5_wifi.py
+++ python/ejemplo5_wifi.py
@@ -1,3 +1,15 @@
 # ejemplo 5 - wifi
 
-# https://github.com/elecfreaks/pxt-esp8266iot/blob/master/ESP8266.ts
+# https://github.com/lionyhw/EF_Produce_MicroPython 
+
+from IoTbit import IOT
+
+
+def test_wifi():
+
+    iot = IOT()
+    if iot.connectWIFI("DIGIFIBRA-4BB8_plus","qazxcvbgtrewsdf26"):
+        display.show(Image.YES)
+    else:
+        display.show(Image.NO)
+  
